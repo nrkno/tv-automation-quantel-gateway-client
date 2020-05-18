538b7a5bed28bc944f1d8d2bd106ee5e4c743aa9
Richard Cartwright <spark@streampunk.media>
2020-05-18 12:33:00 +0100

    chore: add send-coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,25 @@
 module.exports = {
-	preset: 'ts-jest',
-	testEnvironment: 'node'
+	globals: {
+		'ts-jest': {
+			tsConfig: 'tsconfig.json'
+		}
+	},
+	moduleFileExtensions: ['js', 'ts'],
+	transform: {
+		'^.+\\.(ts|tsx)$': 'ts-jest'
+	},
+	testMatch: ['**/__tests__/**/*.spec.(ts|js)'],
+	testPathIgnorePatterns: ['integrationTests'],
+	testEnvironment: 'node',
+	coverageThreshold: {
+		global: {
+			branches: 80,
+			functions: 100,
+			lines: 95,
+			statements: 90
+		}
+	},
+	coverageDirectory: './coverage/',
+	collectCoverage: true,
+	preset: 'ts-jest'
 }
